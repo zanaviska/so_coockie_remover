25608556d7250e860997e501a1cf74e3fbf01443
Andras <dlink.uaua@gmail.com>
2021-09-16 19:18:06 +0300

    initial commit

---
+++ content_script.js
@@ -0,0 +1,2 @@
+var element = document.querySelector("body > div.ff-sans.ps-fixed.z-nav-fixed.ws4.sm\\:w-auto.p32.sm\\:p16.bg-black-750.fc-white.bar-lg.b16.l16.r16.js-consent-banner");
+element.parentElement.removeChild(element);
